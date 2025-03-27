5335e470d69ac2b03c8f39c9966107aa3bb4e222
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-03-27 14:38:12 +0800

    feat: Testing panic messages

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -44,6 +44,13 @@ fn test_match_with_message() -> Result<(), &'static str> {
     Err("error")
 }
 
+#[apply(should_match, pattern = Err("error"))]
+#[test]
+#[should_panic(expected = "Expected to match `Err(\"error\")`")]
+fn test_match_panic() -> Result<(), &'static str> {
+    Ok(())
+}
+
 // Testing shortcut macros
 
 #[test]
@@ -52,10 +59,24 @@ fn test_ok() -> Result<(), &'static str> {
     Ok(())
 }
 
+#[apply(should_ok)]
+#[test]
+#[should_panic(expected = "Expected `Ok`, but got `Err`")]
+fn test_ok_panic() -> Result<(), &'static str> {
+    Err("error")
+}
+
 #[test]
 #[apply(should_err)]
 fn test_err() -> Result<(), &'static str> {
     Err("error")
+}
+
+#[apply(should_err)]
+#[test]
+#[should_panic(expected = "Expected `Err`, but got `Ok`")]
+fn test_err_panic() -> Result<(), &'static str> {
+    Ok(())
 }
 
 #[test]
@@ -64,10 +85,24 @@ fn test_some() -> Option<()> {
     Some(())
 }
 
+#[apply(should_some)]
+#[test]
+#[should_panic(expected = "Expected `Some`, but got `None`")]
+fn test_some_panic() -> Option<()> {
+    None
+}
+
 #[test]
 #[apply(should_none)]
 fn test_none() -> Option<()> {
     None
+}
+
+#[apply(should_none)]
+#[test]
+#[should_panic(expected = "Expected `None`, but got `Some`")]
+fn test_none_panic() -> Option<()> {
+    Some(())
 }
 
 // Testing custom enums
